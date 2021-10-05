fd8c99d4cb93d21c9ade280b65cf9283f09e2c6b
angflx-oss <angelafelix98@gmail.com>
2021-10-05 15:12:49 -0600

    Final

---
+++ blog/angular-y-scully.md
@@ -0,0 +1,8 @@
+---
+title: Angular y Scully
+description: 'Como construir un blog con Angular y Scully'
+published: true
+---
+
+# Angular y Scully
+Si
---
+++ scully.myBlog.config.ts
@@ -0,0 +1,14 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "myBlog",
+  outDir: './dist/static',
+  routes: {
+    '/posts/:id': {
+      type: 'contentFolder',
+      id: {
+        folder: "./posts"
+      }
+    },    
+  }
+};
